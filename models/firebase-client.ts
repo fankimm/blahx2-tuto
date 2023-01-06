225d9760f8f5e061edfc92d8c6224a25fbaae997
Jihwan Kim <jihwan.kim@purple.io>
2023-01-06 12:45:53 +0900

    add firebase-client.ts

---
+++ models/firebase-client.ts
@@ -0,0 +1,38 @@
+import { Auth, getAuth } from 'firebase/auth';
+import { getApps, initializeApp } from 'firebase/app';
+import getConfig from 'next/config';
+
+const { publicRuntimeConfig } = getConfig();
+const { apiKey, authDomain, projectId } = publicRuntimeConfig;
+const FirebaseCredentials = {
+  apiKey,
+  authDomain,
+  projectId,
+};
+
+export default class FirebaseClient {
+  private static instance: FirebaseClient;
+
+  private auth: Auth;
+
+  public constructor() {
+    const apps = getApps();
+    if (apps.length === 0) {
+      console.info('firebase client init start');
+      initializeApp(FirebaseCredentials);
+    }
+    this.auth = getAuth();
+    console.info('firebase auth');
+  }
+
+  public static getInstance(): FirebaseClient {
+    if (!FirebaseClient.instance) {
+      FirebaseClient.instance = new FirebaseClient();
+    }
+    return FirebaseClient.instance;
+  }
+
+  public get Auth(): Auth {
+    return this.auth;
+  }
+}
