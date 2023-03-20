135318b10163cc4ee6f010360d98b2b801f4726c
Jihwan Kim <jihwan.kim@purple.io>
2023-03-20 11:04:46 +0900

    feat : 상세 뒤로가기 버튼

--- pages/[screenName]/[messageId].tsx
+++ pages/[screenName]/[messageId].tsx
@@ -1,8 +1,10 @@
 /* eslint-disable no-return-await */
 import { GetServerSideProps, NextPage } from 'next';
-import { Avatar, Box, Flex, Text } from '@chakra-ui/react';
+import { Avatar, Box, Button, Flex, Text } from '@chakra-ui/react';
 import { useState } from 'react';
 import axios, { AxiosResponse } from 'axios';
+import Link from 'next/link';
+import { ChevronLeftIcon } from '@chakra-ui/icons';
 import ServiceLayout from '@/components/service_layout';
 import { useAuth } from '@/contexts/auth_user.context';
 import { InAuthUser } from '@/models/in_auth_user';
@@ -12,9 +14,10 @@ import { InMessage } from '@/models/message/in_message';
 interface Props {
   userInfo: InAuthUser | null;
   messageData: InMessage | null;
+  screenName: string;
 }
 
-const MessagePage: NextPage<Props> = function ({ userInfo, messageData: initMsgData }) {
+const MessagePage: NextPage<Props> = function ({ userInfo, messageData: initMsgData, screenName }) {
   const [messageData, setMessageData] = useState<null | InMessage>(initMsgData);
   const { authUser } = useAuth();
   async function fetchMessageInfo({ uid, messageId }: { uid: string; messageId: string }) {
@@ -35,8 +38,15 @@ const MessagePage: NextPage<Props> = function ({ userInfo, messageData: initMsgD
     return <p>메시지 정보가 없습니다.</p>;
   }
   return (
-    <ServiceLayout title={`${userInfo.displayName}의 홈`} minH="100vh" backgroundColor="gray.50">
+    <ServiceLayout title={`${screenName}의 홈`} minH="100vh" backgroundColor="gray.50">
       <Box maxW="md" mx="auto" pt="6">
+        <Link href={`/${screenName}`}>
+          <a>
+            <Button leftIcon={<ChevronLeftIcon />} mb="2" fontSize="sm">
+              {screenName} 홈으로
+            </Button>
+          </a>
+        </Link>
         <Box borderWidth="1px" borderRadius="lg" overflow="hidden" mb="2" bg="white">
           <Flex p="6">
             <Avatar size="lg" src={userInfo.photoURL ?? 'https://bit.ly/broken-link'} mr="2" />
@@ -68,6 +78,7 @@ export const getServerSideProps: GetServerSideProps<Props> = async ({ query }) =
       props: {
         userInfo: null,
         messageData: null,
+        screenName: '',
       },
     };
   }
@@ -76,6 +87,7 @@ export const getServerSideProps: GetServerSideProps<Props> = async ({ query }) =
       props: {
         userInfo: null,
         messageData: null,
+        screenName: '',
       },
     };
   }
@@ -85,11 +97,13 @@ export const getServerSideProps: GetServerSideProps<Props> = async ({ query }) =
     const port = process.env.PORT || '3000';
     const baseUrl = `${protocol}://${host}:${port}`;
     const userInfoRes: AxiosResponse<InAuthUser> = await axios(`${baseUrl}/api/user.info/${screenName}`);
+    const screenNameToStr = Array.isArray(screenName) ? screenName[0] : screenName;
     if (userInfoRes.status !== 200 || userInfoRes.data === undefined || userInfoRes.data.uid === undefined) {
       return {
         props: {
           userInfo: null,
           messageData: null,
+          screenName: screenNameToStr,
         },
       };
     }
@@ -100,6 +114,7 @@ export const getServerSideProps: GetServerSideProps<Props> = async ({ query }) =
       props: {
         userInfo: userInfoRes.data,
         messageData: messageInfoRes.status !== 200 || messageInfoRes.data === undefined ? null : messageInfoRes.data,
+        screenName: screenNameToStr,
       },
     };
   } catch (err) {
@@ -108,6 +123,7 @@ export const getServerSideProps: GetServerSideProps<Props> = async ({ query }) =
       props: {
         userInfo: null,
         messageData: null,
+        screenName: '',
       },
     };
   }
